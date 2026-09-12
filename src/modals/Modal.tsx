baa71926ad2704555a15828265c1ad6f7cb425e8
Luke Stainer <lstainer2002@gmail.com>
2026-09-12 21:37:35 +0930

    Create modal component

---
+++ src/modals/Modal.tsx
@@ -0,0 +1,42 @@
+import type React from "react";
+import { Card } from "../components/Card";
+import { useModal } from "../hooks/useModal";
+import { useMemo } from "react";
+import Heading from "../components/Heading";
+import { Icon } from "../components/Icon";
+
+interface ModalProps {
+  title: string;
+}
+
+export const Modal = ({
+  title,
+  children,
+}: React.PropsWithChildren<ModalProps>) => {
+  const { closeModal } = useModal();
+
+  const heading = useMemo(() => {
+    return (
+      <div className="flex w-full flex-row justify-between">
+        <Heading variant="4XL">{title}</Heading>
+        <Icon
+          name="close"
+          className="text-primary-text cursor-pointer"
+          onClick={() => {
+            closeModal();
+          }}
+          size={36}
+        />
+      </div>
+    );
+  }, [closeModal, title]);
+
+  return (
+    <Card className="h-full max-h-full overflow-hidden">
+      <div className="flex h-full min-h-0 flex-col items-center gap-8 p-4">
+        {heading}
+        <div className="flex min-h-0 w-full flex-row gap-4">{children}</div>
+      </div>
+    </Card>
+  );
+};
