120247b63c7c51fe12a548242e21b52665f77f09
Luke Stainer <lstainer2002@gmail.com>
2026-09-14 00:06:42 +0930

    Use CSS grid for slideContent

--- src/slide/SlideContent.tsx
+++ src/slide/SlideContent.tsx
@@ -7,6 +7,7 @@ import { useStreamingHistory } from "../hooks/useStreamingHistory";
 import { debounce } from "lodash";
 import { registerMonacoTransformTypes } from "../helpers/monacoTransformTypes";
 import { Pill } from "../components/Pill";
+import "../assets/css/SlideContent.css";
 import ReactJson from "@microlink/react-json-view";
 import type { ResultRequest, ResultResponse } from "../types/WorkerMessage";
 
@@ -98,72 +99,63 @@ export const SlideContent = ({
   }, [result]);
 
   return (
-    <Card className="flex h-full min-h-0 flex-col">
-      <div className="flex min-h-0 min-w-0 flex-1 flex-col gap-2">
-        <Heading variant="4XL">{title}</Heading>
-        <div className="flex flex-col">
-          <Heading variant="XL">Description</Heading>
-          <Textarea
-            className="outline-tertiary-highlight border-primary-surface-highlight bg-secondary-surface text-primary-text outline-primary-surface-highlight w-full rounded-lg p-2 outline-1"
-            defaultValue={description}
+    <Card className="slideContent flex h-full flex-col">
+      <Heading className="header" variant="4XL">
+        {title}
+      </Heading>
+      <div className="description flex flex-col gap-1">
+        <Heading variant="XL">Description</Heading>
+        <Textarea
+          className="outline-tertiary-highlight border-primary-surface-highlight bg-secondary-surface text-primary-text outline-primary-surface-highlight w-full rounded-lg p-2 outline-1"
+          defaultValue={description}
+        />
+      </div>
+      <div className="input flex min-h-0 w-full flex-col">
+        <Heading variant="L">Input</Heading>
+        <Editor
+          theme="vs-dark"
+          className="outline-tertiary-highlight bg-primary-surface absolute h-full rounded-l"
+          options={{
+            minimap: { enabled: false },
+            automaticLayout: true,
+            fontSize: 13,
+          }}
+          defaultValue={editorContent}
+          defaultLanguage="javascript"
+          beforeMount={(monaco) => {
+            registerMonacoTransformTypes(monaco);
+          }}
+          onChange={(value) => {
+            if (!value) {
+              return;
+            }
+
+            debouncedSetSlideConfiguration(value);
+          }}
+        />
+      </div>
+      <div className="result flex w-full flex-col overflow-auto">
+        <Heading variant="L">Result</Heading>
+        <div className="max-h-full overflow-y-scroll rounded-lg">
+          <ReactJson
+            quotesOnKeys={false}
+            indentWidth={2}
+            src={result}
+            theme={"eighties"}
+            name={false}
+            collapsed={2}
+            displayArrayKey={false}
+            style={{
+              backgroundColor: "var(--color-primary-surface)",
+            }}
           />
         </div>
-        <Heading className="w-full" variant="XL">
-          Transformation
+      </div>
+      <div className="columns flow-row flex gap-2">
+        <Heading className="min-w-fit" variant="XL">
+          Columns to showcase:
         </Heading>
-        <div className="flex min-h-0 flex-1 flex-col gap-0.5">
-          <div className="flex min-h-0 flex-1 flex-row gap-1">
-            <div className="col-span-1 flex min-h-0 w-full flex-1 flex-col">
-              <Heading variant="L">Input</Heading>
-              <Editor
-                theme="vs-dark"
-                className="outline-tertiary-highlight bg-primary-surface absolute h-full rounded-l p-2"
-                options={{
-                  minimap: { enabled: false },
-                  automaticLayout: true,
-                  fontSize: 13,
-                }}
-                defaultValue={editorContent}
-                defaultLanguage="javascript"
-                beforeMount={(monaco) => {
-                  registerMonacoTransformTypes(monaco);
-                }}
-                onChange={(value) => {
-                  if (!value) {
-                    return;
-                  }
-
-                  debouncedSetSlideConfiguration(value);
-                }}
-              />
-            </div>
-            <div className="col-span-1 flex min-h-0 w-full flex-1 flex-col">
-              <Heading variant="L">Result</Heading>
-              <div className="max-h-full overflow-y-scroll rounded-lg">
-                <ReactJson
-                  quotesOnKeys={false}
-                  indentWidth={2}
-                  src={result}
-                  theme={"eighties"}
-                  name={false}
-                  collapsed={2}
-                  displayArrayKey={false}
-                  style={{
-                    backgroundColor: "var(--color-primary-surface)",
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flow-row flex w-full min-w-0 gap-2">
-          <Heading className="min-w-fit" variant="XL">
-            Columns to showcase:
-          </Heading>
-          <div className="flex flex-row flex-wrap gap-2">
-            {columnsToShowcase}
-          </div>
-        </div>
+        <div className="flex flex-row flex-wrap gap-2">{columnsToShowcase}</div>
       </div>
     </Card>
   );
